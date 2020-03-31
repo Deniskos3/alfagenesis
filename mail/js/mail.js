02a1732cefab2ee7f843c70b9d91c9dd19521947
Deniskos1 <Deniskos200029@users.noreply.github.com>
2020-03-31 21:26:34 +0300

    forms is working

---
+++ mail/js/mail.js
@@ -0,0 +1,28 @@
+
+// Отправка данных на сервер
+$('#callback').trigger('reset');
+$("#form-send").on("click",function(){
+    console.log("3");
+    "use strict";
+   $("#callback").on("click", function(e){
+    console.log("333");
+    e.preventDefault();
+    $.ajax({
+      url: 'mail/php/send.php',
+      type: 'POST',
+      contentType: false,
+      processData: false,
+      data: new FormData(this),
+      success: function(msg) {
+        if (msg == 'ok') {
+          console.log('Сообщение отправлено');
+          $('#callback').trigger('reset'); // очистка формы
+        } else {
+            console.log('Сообщение без файла');
+        }
+      }
+    });
+  });
+});
+
+
---
+++ mail/php/send.php
@@ -0,0 +1,68 @@
+<?php
+
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$callback_comment = $_POST['callback_comment'];
+
+
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $msg = "ok";
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";                                          
+    $mail->SMTPAuth   = true;
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера GMAIL
+    $mail->Username   = 'alfo.vork@mail.ru'; // Логин на почте
+    $mail->Password   = 'hfphf,jnrf1'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('alfo.vork@mail.ru', 'admin'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('info@alfagenesis.ru');  
+    // Прикрипление файлов к письму
+if (!empty($_FILES['myfile']['name'][0])) {
+    for ($ct = 0; $ct < count($_FILES['myfile']['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['myfile']['name'][$ct]));
+        $filename = $_FILES['myfile']['name'][$ct];
+        if (move_uploaded_file($_FILES['myfile']['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+        } else {
+            $msg .= 'Неудалось прикрепить файл ' . $uploadfile;
+        }
+    }   
+}
+
+
+        // -----------------------
+        // Само письмо
+        // -----------------------
+        $mail->isHTML(true);
+    
+        $mail->Subject = 'Заявка для обсуждения проектиа';
+        $mail->Body    = "<b>Имя:</b> $name <br>
+        <b>Номер:</b> $phone<br>
+        <b>Сообщение:</b><br>$callback_comment<br>";
+
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {
+    echo "$msg";
+} else {
+echo "Сообщение не было отправлено. Неверно указаны настройки вашего тф";
+}
+
+} catch (Exception $e) {
+    echo "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+?>
